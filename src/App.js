1323ee1d702a4724e50fde72c40c1dad8ed8e203
Michael Lai <mrlai7782@gmail.com>
2021-04-21 09:49:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cart" id="cart">
+              Cart
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
+const Home = () => {
+  return <div className="home">home</div>;
+};
+
+const Shop = () => {
+  return <div>shop</div>;
+};
+
+const About = () => {
+  return <div>about</div>;
+};
+
+const Cart = () => {
+  return <div>cart</div>;
+};
