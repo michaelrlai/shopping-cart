2daab3f68bb38de99cf37e7bb9e98d98e4472b78
Michael Lai <mrlai7782@gmail.com>
2021-04-30 09:52:32 -0700

    update

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,55 +1,87 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
 
 const Shop = (props) => {
+  const { path, url } = useRouteMatch();
   const { products } = props;
   return (
-    <Switch>
-      <Route path="/">
-        <div className="shop-container">
-          <div className="shop-nav-container">
-            <ul>
-              <li id="shop-nav-title">Shop /</li>
-              <li>Bracelets</li>
-              <li>Necklaces</li>
-              <li>Rings</li>
-            </ul>
-          </div>
-          <div className="shop-container-right">
-            <div className="shop-container-right-banner">
-              <img
-                src="https://images.unsplash.com/photo-1547738238-5ddb16bef15f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-                alt=""
-              />
-              <img
-                src="https://images.unsplash.com/photo-1535556116002-6281ff3e9f36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80"
-                alt=""
-              />
-              <img
-                src="https://images.unsplash.com/photo-1608543837489-0fab463c925e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-                alt=""
-              />
-            </div>
-            <div className="products-container-title">Shop All</div>
-            <div className="products-container">
-              {products.map((product) => (
-                <div className="product-container" key={product.name}>
-                  <Link to={`/shop/products/${product.url}`}>
-                    <img className="product-image" src={product.src} alt="" />
-                  </Link>
-                  <div className="product-caption">
-                    <Link to={`/shop/products/${product.url}`}>
-                      <div className="product-name">{product.name}</div>
-                    </Link>
-                    <div className="product-price">{`$${product.price}`}</div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+    <div className="shop-container">
+      <div className="shop-nav-container">
+        <ul>
+          <li id="shop-nav-title">Shop /</li>
+          <li>
+            <Link to={`${url}/bracelets`}>Bracelets</Link>
+          </li>
+          <li>Necklaces</li>
+          <li>Rings</li>
+        </ul>
+      </div>
+      <div className="shop-container-right">
+        <div className="shop-container-right-banner">
+          <img
+            src="https://images.unsplash.com/photo-1547738238-5ddb16bef15f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1535556116002-6281ff3e9f36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1608543837489-0fab463c925e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+            alt=""
+          />
         </div>
-      </Route>
-    </Switch>
+        <div className="products-container-title">Shop All</div>
+        <Switch>
+          <Route exact path={path}>
+            <ShowProducts products={products} />
+          </Route>
+          <Route path={`${path}/bracelets`}>
+            <div>HI</div>
+            <Bracelets products={products} />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   );
 };
 
 export default Shop;
+
+const Bracelets = (props) => {
+  const { products } = props;
+  const [bracelets, setBracelets] = useState([]);
+
+  useEffect(() => {
+    setBracelets(
+      products.filter((product) => {
+        if (product.type === "bracelet") {
+          return product;
+        }
+      })
+    );
+  }, []);
+
+  return <ShowProducts products={bracelets} />;
+};
+
+const ShowProducts = (props) => {
+  const { products } = props;
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <div className="product-container" key={product.name}>
+          <Link to={`/shop/products/${product.url}`}>
+            <img className="product-image" src={product.src} alt="" />
+          </Link>
+          <div className="product-caption">
+            <Link to={`/shop/products/${product.url}`}>
+              <div className="product-name">{product.name}</div>
+            </Link>
+            <div className="product-price">{`$${product.price}`}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
